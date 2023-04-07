dbd749212ccae195b123f0b419212128dfd0668f
elise-227 <elise.boos@duke.edu>
2023-04-06 21:38:16 -0400

    finished predict code w all rasters

--- Predictions.R
+++ Predictions.R
@@ -1,12 +1,21 @@
 require(raster)
 require(tidyverse)
+require(sf)
+
 DEM <- raster("./CroppedRasters/DEM.tif")
 FH <- raster("./CroppedRasters/ForestHeight.tif")
 TRI <- raster("./CroppedRasters/TRI.tif")
+FL <- raster("./CroppedRasters/EliseData/FocalForestLoss.tif")
+names(FL) <- "ForestLoss"
+FP <- raster("./CroppedRasters/EliseData/FocalForest.tif")
+names(FP) <- "ForestTot"
 
-blop <- stack(DEM,TRI,FH)
+blop <- stack(DEM,TRI,FH, FL, FP)
 
 plot(blop)
+
+#finding the mean of the ranges 
+# mean(c(0.1802, 0.2034, 0.2491, 0.1197,0.0437, 0.0778,0.3643,0.0892, 0.0564))
 
 
 ef <- data.frame(coordinates(blop),
@@ -19,8 +28,14 @@ ef <- data.frame(coordinates(blop),
 
 ef <- ef %>% na.omit
 
-ef <- ef %>% mutate(pred = 3.5*(TRI) -2.1*(DEM) + 2*(ForestHeight))
-
+ef <- ef %>% mutate(pred = 3.5*(TRI) -2.1*(DEM) + 2*(ForestHeight) + 3*((ForestLoss+.01)/(ForestTot+0.01)))
 
 pred <- rasterFromXYZ(ef)
+
+bound <- read_sf("./CroppedRasters/EliseData/comatsa_sud_boundary.shp")
+crs(bound)
+
+bound2 <- st_transform(bound, crs(DEM))
+
 plot(pred$pred)
+plot(bound2$geometry, add = TRUE)
